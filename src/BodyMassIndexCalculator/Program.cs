c779851ddc0414f2fbafbe0f4b745751bacfb8f6
Enrico Massone <enrico.massone@hotmail.it>
2023-04-24 22:17:50 +0200

    completed

--- src/BodyMassIndexCalculator/Program.cs
+++ src/BodyMassIndexCalculator/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace BodyMassIndexCalculator
+﻿using System.Globalization;
+using static BodyMassIndexCalculator.Functions;
+
+namespace BodyMassIndexCalculator
 {
   public static class Program
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World!");
+      Run(ReadInputFromUser, WriteToConsole);
     }
+
+    private static void Run(Func<string, double> reader, Action<HealthCondition> writer)
+    {
+      var height = reader("Please enter your height in meters");
+      var weight = reader("Please enter your weight in kilograms");
+
+      var bodyMassIndex = ComputeBodyMassIndex(
+        new Weight(weight),
+        new Height(height)
+      );
+
+      var healthCondition = ToHealthCondition(bodyMassIndex);
+
+      writer(healthCondition);
+    }
+
+    private static double ReadInputFromUser(string prompt)
+    {
+      Console.WriteLine(prompt);
+      var value = Console.ReadLine() ?? string.Empty;
+      return double.Parse(value, CultureInfo.CurrentCulture);
+    }
+
+    private static void WriteToConsole(HealthCondition healthCondition) =>
+      Console.WriteLine($"Your current condition is: {healthCondition}");
   }
 }
